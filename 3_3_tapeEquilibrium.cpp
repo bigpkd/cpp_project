110827fe6e6620bf707ebff6151d4ff590a9b5ca
bigpkd <bigpkd@yahoo.fr>
2021-02-08 07:19:18 -0500

    tapeEquilibrium

--- 3_3_tapeEquilibrium.cpp
+++ 3_3_tapeEquilibrium.cpp
@@ -18,15 +18,32 @@ void displayVector(const std::vector<T> vector) {
     cout << endl;
 }
 
-int solution(vector<int> &A) {
+int solution_2(vector<int> &A) {    // 92% 85% 100%  O(N)
     int res = INT_MAX;
-    int after = accumulate(A.begin(), A.end(), 0);
-    int prior = 0;
-    for (int val : A) {
-        prior += val;
-        after -= val;
+    if (A.size() == 2) {
+        res = abs(A.front() - A.back());
+    } else if (A.size() > 2) {
+        int after = accumulate(A.begin(), A.end(), 0);
+        int prior = 0;
+        for (int val : A) {
+            prior += val;
+            after -= val;
+            int difference = abs(prior - after);
+            res = difference < res ? difference : res;
+        }
+    }
+    return res;
+}
+
+int solution(vector<int> &A) {  // 100% 100% 100%  O(N)
+    int res = INT_MAX;
+    int after = accumulate(A.begin(), A.end(), 0) - A.front();
+    int prior = A.front();
+    for (size_t i = 1; i < A.size(); ++i) {
         int difference = abs(prior - after);
         res = difference < res ? difference : res;
+        prior += A.at(i);
+        after -= A.at(i);
     }
     return res;
 }
@@ -40,8 +57,8 @@ int main() {
 }
 /** Lesson 3
 3. TapeEquilibrium : Minimize the value |(A[0] + ... + A[P-1]) - (A[P] + ... + A[N-1])|.
-Task Score : 84%
-Correctness : 71%
-Performance : 100%
+Task Score : 100%
+Correctness : 100%
+Performance : 100%  O(N)
 Task description : https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
  */
