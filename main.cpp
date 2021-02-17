091a83b85df36bdba1a87232b6cadb387234ecfc
bigpkd <bigpkd@yahoo.fr>
2021-02-17 03:38:10 -0500

    main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
@@ -30,14 +31,27 @@ void displayMap(std::map<T, U> map) {
 }
 
 int solution(vector<int> &A) {
-    return set<int>(A.begin(), A.end()).size();
+    sort(A.begin(), A.end());
+    set<int> potentialMaxProductsOfThree;
+    size_t positionsOfExtremes[]{0, 1, 2, A.size() - 3, A.size() - 2, A.size() - 1};
+    set<size_t> range(positionsOfExtremes, positionsOfExtremes + sizeof(positionsOfExtremes) / sizeof(size_t));
+    for (size_t i : range) {
+        for (size_t j : range) {
+            if (i == j) continue;
+            for (size_t k : range) {
+                if (i == k || j == k) continue;
+                potentialMaxProductsOfThree.emplace(A.at(i) * A.at(j) * A.at(k));
+            }
+        }
+    }
+    return *potentialMaxProductsOfThree.rbegin();
 }
 
 int main() {
-    int myInts[]{1, 2, 5, 3, 1, 3};    // 2
-    int myInts1[]{3, 3, 3, 5, 4};    // 3
-    int myInts2[]{6, 5, 5, 6, 2, 2};   // 4
-    int myInts3[]{2, 1, 1, 2, 3, 1};    // 2
+    int myInts[]{-3, 1, 2, -2, 5, 6};   // 60
+    int myInts1[]{-5, 5, -5, 4};    // 125
+    int myInts2[]{-1, -1, -3, -5, 0};    // 0
+    int myInts3[]{-3, -1, -2, -2, -5, -6};    // -4
     vector<int> v(myInts, myInts + sizeof(myInts) / sizeof(int));
     vector<int> v1(myInts1, myInts1 + sizeof(myInts1) / sizeof(int));
     vector<int> v2(myInts2, myInts2 + sizeof(myInts2) / sizeof(int));
@@ -54,6 +68,6 @@ int main() {
 2. MaxProductOfThree : Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).
 Task Score : 100%
 Correctness : 100%
-Performance : 100%  O(N*log(N)) or O(N)
+Performance : 100%  O(N*log(N))
 Task description : https://app.codility.com/programmers/lessons/6-sorting/max_product_of_three/
  */
