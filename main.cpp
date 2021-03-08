50a0c68378d4b05ecd1748aac3b950e9cf8ca19c
bigpkd <bigpkd@yahoo.fr>
2021-03-07 19:21:24 -0500

    main

--- main.cpp
+++ main.cpp
@@ -32,83 +32,65 @@ void displayMap(std::map<T, U> map) {
     cout << endl;
 }
 
-int slow_max_slice(vector<int> &A) {  /** O(N^3), analyze all slices */
+int solution_9(vector<int> &A) {
+    if (A.size() < 3) return 0;
+    vector<int> prefixSums(A.begin(), A.end());
+    for (size_t p = 1; p < A.size(); ++p) {
+        prefixSums.at(p) = prefixSums.at(p - 1) + prefixSums.at(p);
+    }
     int res(0);
-    for (size_t p = 0; p < A.size(); ++p) {
-        for (size_t q = p; q < A.size(); ++q) {
-            int sum(0);
-            for (size_t i = p; i < q + 1; ++i) {
-                sum += A.at(i);
+    for (size_t p = 0; p < A.size() - 2; ++p) {
+        for (size_t q = p + 2; q < A.size(); ++q) {
+            for (size_t i = p + 1; i < q; ++i) {
+                int leftSum = prefixSums.at(i - 1) - prefixSums.at(p);
+                int rightSum = prefixSums.at(q - 1) - prefixSums.at(i);
+                res = max(res, leftSum + rightSum);
             }
-            res = max(res, sum);
         }
     }
     return res;
-}
-int quadratic_max_slice_1(vector<int> &A) {  /** O(N²), use prefix sums */
-    vector<int> prefixSum(A.size() + 1);
-    for (size_t p = 1; p < A.size() + 1; ++p) {
-        prefixSum.at(p) = A.at(p - 1);
-        prefixSum.at(p) = prefixSum.at(p - 1) + prefixSum.at(p);
-    }
-    int res(0);
-    for (size_t p = 0; p < A.size(); ++p) {
-        for (size_t q = p; q < A.size(); ++q) {
-            int sum(prefixSum.at(q + 1) - prefixSum.at(p));
-            res = max(res, sum);
-        }
-    }
-    return res;
-}
-int quadratic_max_slice_2(vector<int> &A) {  /** O(N²), suppress the third loop */
-    int res(0);
-    for (size_t p = 0; p < A.size(); ++p) {
-        int sum(0);
-        for (size_t q = p; q < A.size(); ++q) {
-            sum += A.at(q);
-            res = max(res, sum);
-        }
-    }
-    return res;
-}
-int golden_max_slice(vector<int> &A) {  /** O(N), largest sum ending in each position */
-    int maxEnding(0), maxSlice(0);
-    for (const int &value : A) {
-        maxEnding = max(0, maxEnding + value);
-        maxSlice = max(maxSlice, maxEnding);
-    }
-    return maxSlice;
 }
 
 int solution(vector<int> &A) {
-    int res = INT_MIN;
-//    res = slow_max_slice(A);
-//    res = quadratic_max_slice_1(A);
-//    res = quadratic_max_slice_2(A);
-    res = golden_max_slice(A);
+    if (A.size() < 3) return 0;
+    vector<int> prefixSums(A.begin(), A.end());
+    for (size_t p = 1; p < A.size(); ++p) {
+        prefixSums.at(p) = prefixSums.at(p - 1) + prefixSums.at(p);
+    }
+    int res(0);
+    for (size_t p = 0; p < A.size() - 2; ++p) {
+        for (size_t q = p + 2; q < A.size(); ++q) {
+            for (size_t i = p + 1; i < q; ++i) {
+                int leftSum = prefixSums.at(i - 1) - prefixSums.at(p);
+                int rightSum = prefixSums.at(q - 1) - prefixSums.at(i);
+                res = max(res, leftSum + rightSum);
+            }
+        }
+    }
     return res;
 }
 
 int main() {
-    int myInts[]{5, -7, 3, 5, -2, 4, -1};       //  10
-    int myInts1[]{8, 8, 5, 7, 9, 8, 7, 4, 8};   //  64
-    int myInts2[]{4, 6, 6, 6, 6, 8, 8};         //  44
-//    int myInts3[]{3, 1, 2, 2, 5, 6};    //
+    int myInts[]{3, 2, 6, -1, 4, 5, -1, 2};       //  17
+    int myInts1[]{8, 8, 5, 7, 9, 8, 7, 4, 8};   //  44
+    int myInts2[]{4, 6, 6, 6, 6, 8, 8};         //  26
+    int myInts3[]{3, 4, 5};    //   0
+//    int myInts3[]{0, 10, -5, -2, 0};    //    10
     vector<int> v(myInts, myInts + sizeof(myInts) / sizeof(int));
     vector<int> v1(myInts1, myInts1 + sizeof(myInts1) / sizeof(int));
     vector<int> v2(myInts2, myInts2 + sizeof(myInts2) / sizeof(int));
-//    vector<int> v3(myInts3, myInts3 + sizeof(myInts3) / sizeof(int));
+    vector<int> v3(myInts3, myInts3 + sizeof(myInts3) / sizeof(int));
 
     display(solution(v));
     display(solution(v1));
     display(solution(v2));
-//    display(solution(v3));
+    display(solution(v3));
 
     return 0;
 }
 
 /** Lesson 9
-0. Maximum slice problem : given a sequence of n integers a_0, a_1, . . . , a_n−1, find the slice with the
+0. MaximumSlice : given a sequence of n integers a_0, a_1, . . . , a_n−1, find the slice with the
  largest sum. More precisely, we are looking for two indices p, q such that the total a_p+ a_p+1+ . . . + a_q is maximal.
  We assume that the slice can be empty and its sum equals 0.
 
