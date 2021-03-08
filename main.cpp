608ab01f8d85bb23ba8d2fa23a057f39cee07951
bigpkd <bigpkd@yahoo.fr>
2021-03-07 23:17:16 -0500

    main

--- main.cpp
+++ main.cpp
@@ -32,38 +32,11 @@ void displayMap(std::map<T, U> map) {
     cout << endl;
 }
 
-int solution_9(vector<int> &A) {
-    if (A.size() < 3) return 0;
-    vector<int> prefixSums(A.begin(), A.end());
-    for (size_t p = 1; p < A.size(); ++p) {
-        prefixSums.at(p) = prefixSums.at(p - 1) + prefixSums.at(p);
-    }
-    int res(0);
-    for (size_t p = 0; p < A.size() - 2; ++p) {
-        for (size_t q = p + 2; q < A.size(); ++q) {
-            for (size_t i = p + 1; i < q; ++i) {
-                int leftSum = prefixSums.at(i - 1) - prefixSums.at(p);
-                int rightSum = prefixSums.at(q - 1) - prefixSums.at(i);
-                res = max(res, leftSum + rightSum);
-            }
-        }
-    }
-    return res;
-}
-// https://rafal.io/posts/codility-max-double-slice-sum.html
 int solution(vector<int> &A) {
-    if (A.size() < 3) return 0;
-    vector<int> left(A.size());
-    vector<int> right(A.size());
-    for (size_t i = 1; i < A.size() - 1; ++i) {
-        left.at(i) = max(0, left.at(i - 1) + A.at(i));
-    }
-    for (size_t i = A.size() - 2; i > 0; --i) {
-        right.at(i) = max(0, right.at(i + 1) + A.at(i));
-    }
-    int res(0);
-    for (size_t i = 1; i < A.size() - 1; ++i) {
-        res = max(res, left.at(i - 1) + right.at(i + 1));
+    int res(0), maxEnding(0);
+    for (const int &value : A) {
+        maxEnding = max(0, maxEnding + value);
+        res = max(res, maxEnding);
     }
     return res;
 }
@@ -72,7 +45,7 @@ int main() {
     int myInts[]{3, 2, 6, -1, 4, 5, -1, 2};       //  17
     int myInts1[]{8, 8, 5, 7, 9, 8, 7, 4, 8};   //  44
     int myInts2[]{4, 6, 6, 6, 6, 8, 8};         //  26
-    int myInts3[]{3, 4, 5};    //   0
+    int myInts3[]{23171, 21011, 21123, 21366, 21013, 21367};    //   0
 //    int myInts3[]{0, 10, -5, -2, 0};    //    10
     vector<int> v(myInts, myInts + sizeof(myInts) / sizeof(int));
     vector<int> v1(myInts1, myInts1 + sizeof(myInts1) / sizeof(int));
