20ec62fcdc1afc02fbdf16a43022bcbd0045c775
bigpkd <bigpkd@yahoo.fr>
2021-03-13 17:27:46 -0500

    main

--- main.cpp
+++ main.cpp
@@ -33,19 +33,21 @@ void displayMap(std::map<T, U> map) {
 }
 
 int solution(vector<int> &A) {
-    int res(0);
-    for (size_t i = 0; i < A.size(); ++i) {
-        for (size_t j = 0; j < i; ++j) {
-            res = max(res, max(0, A.at(i) - A.at(j)));
-        }
+    int maxProfit(0);
+    int minPrice(A.empty() ? 0 : A.front());
+    int maxProfitOnThatDay(0);
+    for (int &priceOfTheDay : A) {
+        minPrice = min(minPrice, priceOfTheDay);
+        maxProfitOnThatDay = max(0, priceOfTheDay - minPrice); // The function should return 0 if it was impossible to gain any profit.
+        maxProfit = max(maxProfit, maxProfitOnThatDay);
     }
-    return res;
+    return maxProfit;
 }
 
 int main() {
-    int myInts[]{3, 2, 6, -1, 4, 5, -1, 2};       //  17
-    int myInts1[]{8, 8, 5, 7, 9, 8, 7, 4, 8};   //  44
-    int myInts2[]{4, 6, 6, 6, 6, 8, 8};         //  26
+    int myInts[]{3, 2, 6, -1, 4, 5, -1, 2};     //  6
+    int myInts1[]{8, 8, 5, 7, 9, 8, 7, 4, 8};   //  4
+    int myInts2[]{4, 6, 6, 6, 6, 8, 8};         //  4
     int myInts3[]{23171, 21011, 21123, 21366, 21013, 21367};    //   0
 //    int myInts3[]{0, 10, -5, -2, 0};    //    10
     vector<int> v(myInts, myInts + sizeof(myInts) / sizeof(int));
@@ -53,19 +55,19 @@ int main() {
     vector<int> v2(myInts2, myInts2 + sizeof(myInts2) / sizeof(int));
     vector<int> v3(myInts3, myInts3 + sizeof(myInts3) / sizeof(int));
 
-//    display(solution(v));
-//    display(solution(v1));
-//    display(solution(v2));
-//    display(solution(v3));
+    display(solution(v));
+    display(solution(v1));
+    display(solution(v2));
+    display(solution(v3));
 
     return 0;
 }
 
 /** Lesson 9
 2. MaxProfit : Given a log of stock prices compute the maximum possible earning.
-Task Score : %
-Correctness : %
-Performance : 100%  O()
+Task Score : 100%
+Correctness : 100%
+Performance : 100%  O(N)
 Task description : https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_profit/
 */
 
